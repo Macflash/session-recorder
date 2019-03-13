f7b9aa2a137b7ecdef9d3552197e6ae1865ad5d7
Alex Breyfogle <alexbr@microsoft.com>
2019-03-13 16:01:32 -0700

    Updates

---
+++ precache-manifest.180feaa241dc01e21449ed150ab9aaaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591687a3dcfcffedceff",
+    "url": "/session-recorder/static/js/runtime~main.68756b9b.js"
+  },
+  {
+    "revision": "56bfd03f25ecef49ef05",
+    "url": "/session-recorder/static/js/main.c8708d5d.chunk.js"
+  },
+  {
+    "revision": "f36054a2f6c5bf348fa1",
+    "url": "/session-recorder/static/js/2.4214afab.chunk.js"
+  },
+  {
+    "revision": "56bfd03f25ecef49ef05",
+    "url": "/session-recorder/static/css/main.9589199e.chunk.css"
+  },
+  {
+    "revision": "dc0bba1d000929121227f24da94fb030",
+    "url": "/session-recorder/index.html"
+  }
+];
